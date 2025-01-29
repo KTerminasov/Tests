2653088c924bbee0d72ea22ae1a16bb057759eff
Кирилл Терминасов <kterminasov@gmail.com>
2025-01-29 13:18:58 +0300

    Add Valid_Anagram.py to Neetcode

--- NeetCode/Arrays_and_Hashing/Contains_Duplicate.py
+++ NeetCode/Arrays_and_Hashing/Contains_Duplicate.py
@@ -1,7 +1,5 @@
-'''
-Given an integer array nums, return true if any value appears more than once
-in the array, otherwise return false.
-'''
+"""Given an integer array nums, return true if any value appears more than once
+in the array, otherwise return false."""
 
 
 class Solution:
