2ddf73d3c6b8dabbc8934e82beed01898bcf3c97
Кирилл Терминасов <kterminasov@gmail.com>
2025-08-23 13:59:54 +0300

    Create Source and Quote models

--- quotes/quotes_test_project/quotes/models.py
+++ quotes/quotes_test_project/quotes/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Source(models.Model):
+    """Модель источника цитаты."""
+
+    TAG_CHOICES = [
+        ('Book', 'Книга'),
+        ('Movie', 'Фильм'),
+        ('Series', 'Сериал'),
+        ('Game', 'Игра'),
+        ('Scientific article', 'Научная статья'),
+        ('Other', 'Другое'),
+    ]
+
+    title = models.CharField(max_length=100, verbose_name="Название источника")
+    tag = models.CharField(
+        max_length=20, choices=TAG_CHOICES, verbose_name="Тег"
+    )
+
+    class Meta:
+        unique_together = ['title', 'tag']
+
+
+class Qoute(models.Model):
+    """Модель цитаты."""
+
+    source = models.ForeignKey(
+        Source, on_delete=models.CASCADE,
+        verbose_name="Источник цитаты.",
+        related_name="Цитаты"
+    )
+    quote_text = models.TextField(verbose_name="Текст цитаты")
+    weight = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)], 
+        verbose_name="Вес"
+    )
+    views = models.PositiveIntegerField(default=0, verbose_name="Просмотры")
+    likes = models.PositiveIntegerField(
+        default=0, verbose_name="Количество лайков"
+    )
+    dislikes = models.PositiveIntegerField(
+        default=0, verbose_name="Количество дизлайков"
+    )
+
+    class Meta:
+        unique_together = ['source', 'quote_text']
--- quotes/quotes_test_project/quotes_test_project/settings.py
+++ quotes/quotes_test_project/quotes_test_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'qoutes.apps.QoutesConfig'
+    'quotes.apps.QuotesConfig'
 ]
 
 MIDDLEWARE = [
