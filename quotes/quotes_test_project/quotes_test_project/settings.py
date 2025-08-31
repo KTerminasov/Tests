1ba72974f30797472233bc37322f1444fe200547
Kirill Terminasov <kterminasov@gmail.com>
2025-08-31 03:58:27 +0300

    Collected staticfiles

--- quotes/quotes_test_project/quotes_test_project/settings.py
+++ quotes/quotes_test_project/quotes_test_project/settings.py
@@ -1,19 +1,15 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-o5^@&(pvc(*zb464l^1p)unulu5(o8qn%$76os5_+3(8olbzy^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['kterminasov.pythonanywhere.com', 'localhost', '127.0.1']
 
 
 # Application definition
@@ -107,6 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
